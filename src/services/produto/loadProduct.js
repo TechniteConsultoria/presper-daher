aeea2ca3af542887ae94550114599ca6afec8b5d
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-04 16:42:51 -0300

    now it returns the correct data

--- src/services/produto/loadProduct.js
+++ src/services/produto/loadProduct.js
@@ -5,8 +5,8 @@ import { api, apiWithTenantAndWithToken } from "../api";
 export default async function loadProduct(){
     return await api.get(`produto`)
           .then((response) => {
-            console.log(response)
-            let productData = response.data
+            console.log(response.data.rows)
+            let productData = response.data.rows
             return productData
           });
 }
