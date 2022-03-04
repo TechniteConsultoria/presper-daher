d71162acb1ecc0b084f00b3fc7aaddb5559b39e9
Beatriz Daddea <beatrizchith@gmail.com>
2022-03-04 13:33:08 -0300

    Update Footer.jsx

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -4,24 +4,7 @@ function Footer() {
   return (
     <>
       <footer className="footer">
-        <div className="footer-home">
-          <p>Inicio</p>
-          <a href="/" className="link"> Home</a>
-          <a href="/" className="link"> Cursos</a>
-        </div>
-        <div className="footer-about-us">
-          <p>Sobre Nós</p>
-          <a href="/" className="link"> Informações da empresa</a>
-          <a href="/" className="link"> Contato</a>
-          
-        </div>
-        <div className="footer-support">
-          <p>Suporte</p>
-          <a href="/" className="link"> FAQ</a>
-          <a href="/" className="link"> Telefones </a>
-          <a href="/" className="link"> Chat</a>
-        </div>
-        <br/>
+      
     <div className="footer-info"> 
     <p className="text-footer">
           Ensino EAD © 2022 All Rights Reserved
