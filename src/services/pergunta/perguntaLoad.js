e591552a876a2100132af079a2aba0cbe52d4f65
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-04 10:08:00 -0300

    created questions crud folder

---
+++ src/services/pergunta/perguntaLoad.js
@@ -0,0 +1,12 @@
+import { api, apiWithTenantAndWithToken } from "../api";
+
+
+
+export default async function loadPergunta(){
+    return await api.get(`pergunta`)
+          .then((response) => {
+            console.log(response)
+            let perguntaData = response.data
+            return perguntaData
+          });
+}
