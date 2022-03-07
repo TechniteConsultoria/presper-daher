7181e488824e79d820b1972a6d4c54bba67631c2
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-07 15:20:53 -0300

    added the banner function to delete

--- src/componentes/BannerCard/BannerCard.jsx
+++ src/componentes/BannerCard/BannerCard.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 
 import { BsTrashFill, BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+import bannerDelete from "../../services/banner/bannerDelete";
 
 import "./BannerCard.styles.css";
 
 function BannerCard(props) {
-  async function handleClick() {
-    console.log("Ciclou!");
+
+  async function handleDeleteOne(id){
+    console.log(id)
+    console.log("clicou")
+    await bannerDelete(id)
+    
   }
+
+  async function showOne(id){
+
+    console.log(id)
+
+  }
+
   return (
     <div className="banner-card">
       <div className="banner-title">{props.title}</div>
       <div className="banner-icons">
         <div className="banner-status">
           {props.status ? (
-            <BsEyeFill onClick={handleClick} id="active-icon" />
+            <BsEyeFill onClick={() => showOne(props.id)} id="active-icon" />
           ) : (
-            <BsEyeSlashFill onClick={handleClick} id="inactive-icon" />
+            <BsEyeSlashFill  id="inactive-icon" />
           )}
         </div>
         <div className="banner-action">
-          <BsTrashFill />
+          <BsTrashFill onClick={() => handleDeleteOne(props.id)} />
         </div>
       </div>
     </div>
