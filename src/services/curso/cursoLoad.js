abdb55764864bb95958b37d1542386fbe82f605f
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-07 10:49:55 -0300

    removed the console.log

--- src/services/curso/cursoLoad.js
+++ src/services/curso/cursoLoad.js
@@ -5,7 +5,6 @@ import { api, apiWithTenantAndWithToken } from "../api";
 export default async function cursoLoad(){
     return await api.get(`produto`)
           .then((response) => {
-            console.log(response)
             let produtoData = response.data.rows
             return produtoData
           });
