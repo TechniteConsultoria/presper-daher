cf660cf0ff8540320a62f3536410ea53b46eb4f8
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-08 17:13:43 -0300

    now the function is right

--- src/services/carrinho/addInCart.js
+++ src/services/carrinho/addInCart.js
@@ -3,25 +3,21 @@ import responseHandler from '../../utils/responseHandler';
 import { api } from '../api'
 
 
-export default async function addInCart(product, quantidade){
-  const response = await api.post(`carrinhoProduto/`, { data:{ product, 'quantidade': quantidade} })
+export default async function addInCart(product, cart, quantidade){
+  const response = await api.post(`carrinho/`, {  product, carrinho: cart, 'quantidade': quantidade })
   .then(
     (response) => {
       let status = response.status
       responseHandler(status,"Produto adicionado ao carrinho com sucesso!",  "Erro na adição do produto")
+
       if(response.status == 200){
-        // setUpdate(prevValue => {
-        //   return prevValue+1	
-        //    })
-
-        // console.log("update 1")
-        // console.log(update)
-
         return response.data
       }
+
       else if(response.status == 500){
         toast.error("Problemas com o servidor :(")
       }
+      
     }
   )
   return response
