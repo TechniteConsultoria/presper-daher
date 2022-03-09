f86c2294fb1e3d56b1a79a68f2c02aeccc69fdd3
Jonntz <monteirojonatas23@gmail.com>
2022-03-09 12:34:46 -0300

    we win

--- src/services/curso/cursoLoad.js
+++ src/services/curso/cursoLoad.js
@@ -1,11 +1,12 @@
-import { api, apiWithTenantAndWithToken } from "../api";
+import { api, apiWithoutTenant, apiWithTenantAndWithToken } from "../api";
 
 
 
 export default async function cursoLoad(){
-    return await api.get(`produto`)
+    return await apiWithoutTenant.get(`produto`)
           .then((response) => {
             let produtoData = response.data.rows
+            console.log(produtoData)
             return produtoData
           });
 }
