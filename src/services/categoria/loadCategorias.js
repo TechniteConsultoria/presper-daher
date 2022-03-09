fff0f965d8d6d21200d8680193abd3bbc4f6eac3
Jonntz <monteirojonatas23@gmail.com>
2022-03-09 10:50:18 -0300

    resolving conflict

--- src/services/categoria/loadCategorias.js
+++ src/services/categoria/loadCategorias.js
@@ -1,7 +1,7 @@
-import { api } from "../api"
+import { api, apiWithoutTenant } from "../api"
 
 export default async function loadCategorias(setFunction){
-  return await api.get('categoria').then(
+  return await apiWithoutTenant.get('categoria').then(
       (response) => {
         console.log(response)
         console.log(response.data.rows)
