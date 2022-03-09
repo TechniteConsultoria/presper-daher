dc307cdc050c1fae028204be472a9b72bb25f5fc
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-09 08:59:15 -0300

    wrote the right function to object structure

--- src/utils/makeSumToCarrinho.js
+++ src/utils/makeSumToCarrinho.js
@@ -1,15 +1,12 @@
 import { formatPrice } from "./format";
 
 export default function makeSumToCarrinho(carrinhos){
-    const total = carrinhos.reduce((sumTotal, carrinho) => {
-            carrinho.produtos.filter(
-                ({produto, quantidade}) => {
-                    let precoFormatado =  parseFloat(produto.preco) 
-                    sumTotal += precoFormatado * quantidade
-                }
-            )
+    const total = carrinhos.reduce((sumTotal, { produto, quantidade }) => {
+        let precoFormatado =  parseFloat(produto.preco) 
+        sumTotal += precoFormatado * quantidade
+
         return sumTotal;
-        }, 0)
+    }, 0)
     
     return total
 }
