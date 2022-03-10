b587f5001400ac5fe18f9d96513e6313dfb0913f
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-10 16:10:32 -0300

    removed the tenantId from banner

--- src/services/banner/bannerFind.js
+++ src/services/banner/bannerFind.js
@@ -1,9 +1,9 @@
-import { api, apiWithTenantAndWithToken } from "../api";
+import { api, apiWithoutTenant } from "../api";
 
 
 
 export default async function bannerFind(bannerId){
-    return await api.get(`banner/${bannerId}`)
+    return await apiWithoutTenant.get(`banner/${bannerId}`)
           .then((response) => {
             // console.log(response)
             let bannerData = response.data
--- src/services/banner/bannerLoad.js
+++ src/services/banner/bannerLoad.js
@@ -1,9 +1,9 @@
-import { api, apiWithTenantAndWithToken } from "../api";
+import { api, apiWithoutTenant } from "../api";
 
 
 
 export default async function bannerLoad(){
-    return await api.get(`banner`)
+    return await apiWithoutTenant.get(`banner`)
           .then((response) => {
             let bannerData = response.data.rows
             return bannerData
