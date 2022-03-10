7c32406875a46af818bf5dc473f9ea162e86d3d3
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-10 09:35:40 -0300

    now displayed the course content and the first video, but not displayed the others yet

--- src/pages/WatchCourse/WatchCourse.jsx
+++ src/pages/WatchCourse/WatchCourse.jsx
@@ -16,74 +16,68 @@ import {
   TimeDivider,
   PlaybackRateMenuButton,
   VolumeMenuButton,
+  LoadingSpinner,
 } from "video-react";
 
 import { Tabs, Tab, Container } from "react-bootstrap";
 
 import getIdFromUrl from '../../utils/getIdFromUrl'
-import cursoLoad from "../../services/curso/cursoLoad";
-import cursoFind from "../../services/curso/cursoFind";
+import cursoFindWithRelations from "../../services/curso/cursoFindWithRelations";
+
+import LoadingGif from "../../componentes/LoadingGif";
 
 function WatchCourse() {
-  const mock = {
-    id: 1,
-    title: "Título do curso",
-    videos: [
-      {
-        title: "Aula - 1",
-        time: "19:45",
-        link: "http://mirrorblender.top-ix.org/movies/sintel-1024-surround.mp4",
-      },
-      {
-        title: "Aula - 2",
-        time: "13:42",
-        link: "http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
-      },
-    ],
-  };
 
   const [key, setKey] = useState("content");
-  const [video, setVideo] = useState({});
+  const [video, setVideo] = useState('');
   const [videos, setVideos] = useState([]);
 
-  function setPlayVideo(video) {
-    console.log(video);
-    setVideo(video);
+  function setPlayVideo(newVideo) {
+    console.log(newVideo);
+    setVideo(newVideo);
+    
   }
 
   async function handleLoadCursoModulo(){
     let id  = getIdFromUrl('/watch-course/')
     console.log(id);
-    let product = await cursoFind(id)
+    let product = await cursoFindWithRelations(id)
     console.log(product)
+
+    setVideos(product.produtoModulo)
+    setVideo(product.produtoModulo[0])
+    console.log(product.produtoModulo[0].url)
 
   }
 
   useEffect(() => {
-    setVideos(mock.videos);
     handleLoadCursoModulo()
   }, []);
 
   return (
     <>
-      <div className="player-container">
-        <Player poster="/assets/poster.png">
-          <source src="http://localhost:8142/api/file/download?privateUrl=tenant/883fa309-da4f-4300-85d5-bb59af61a8ac/produto/imagem1/video3.mp4" />
-          {/* <source src="http://mirrorblender.top-ix.org/movies/sintel-1024-surround.mp4" /> */}
-          {/* <source src={video.link} /> */}
+      {video? (
+        <div className="player-container">
+          <Player poster="/assets/poster.png">
+            {/* <source src="http://localhost:8142/api/file/download?privateUrl=tenant/883fa309-da4f-4300-85d5-bb59af61a8ac/produto/imagem1/video3.mp4" /> */}
+            <source src={video.url} />
+            <ControlBar>
+              <ReplayControl seconds={10} order={1.1} />
+              <ForwardControl seconds={30} order={1.2} />
+              <CurrentTimeDisplay order={4.1} />
+              <TimeDivider order={4.2} />
+              <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+              <VolumeMenuButton disabled />
+            </ControlBar>
+          </Player>
+        </div>
+      ):(
+        <LoadingGif/>
+      )}
 
-          <ControlBar>
-            <ReplayControl seconds={10} order={1.1} />
-            <ForwardControl seconds={30} order={1.2} />
-            <CurrentTimeDisplay order={4.1} />
-            <TimeDivider order={4.2} />
-            <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
-            <VolumeMenuButton disabled />
-          </ControlBar>
-        </Player>
-      </div>
+
       <Container>
-        <div className="video-title">{video.title}</div>
+        <div className="video-title">{video.nome}</div>
       </Container>
       <Container className="tab-container">
         <Tabs
@@ -96,13 +90,13 @@ function WatchCourse() {
             <CourseContent videos={videos} onClick={setPlayVideo} />
           </Tab>
           <Tab className="tab-link" eventKey="rate" title="Avaliar">
-            <RateThisCourse courseId={mock.id} />
+            <RateThisCourse courseId={video.id} />
           </Tab>
           <Tab className="tab-link" eventKey="doubts" title="Dúvidas">
-            <SendQuestion courseId={mock.id} />
+            <SendQuestion courseId={video.id} />
           </Tab>
           <Tab className="tab-link" eventKey="share" title="Compartilhar">
-            <ShareCourse courseId={mock.id} />
+            <ShareCourse courseId={video.id} />
           </Tab>
         </Tabs>
       </Container>
