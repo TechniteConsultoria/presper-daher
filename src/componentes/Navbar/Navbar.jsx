539dc63e351ae5ee46c3164303d14ad1e6b0990f
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-15 10:26:23 -0300

    comeback with the old navar

--- src/componentes/Navbar/Navbar.jsx
+++ src/componentes/Navbar/Navbar.jsx
@@ -1,174 +1,3 @@
-// // import { useEffect, useState, useContext } from "react";
-// import { Nav, Navbar, NavDropdown } from "react-bootstrap";
-// import { Container } from "react-bootstrap";
-// import { Form, FormControl } from "react-bootstrap";
-// import { BsFillCartFill } from "react-icons/bs";
-
-// import "../Navbar/Navbar.style.css";
-
-// import { token } from "../../services/api";
-// import loadCategorias from "../../services/categoria/loadCategorias";
-// import logo from "../../assets/project-logo.png";
-
-// import { useCart } from "../../contexts/CartContext";
-// import { CartContext } from "../../contexts/CartContext";
-// import { useAuth } from "../../contexts/AuthContext";
-
-//   const { cart } = useContext(CartContext);
-//   const [ isLogged, setIsLogged ] = useState("");
-//   const [categorias, setCategorias] = useState([]);
-
-// function NavbarComponent() {
-//   const { cart } = useCart();
-//   const { user, logout, role } = useAuth();
-
-//   async function getCategories(){
-//     try {
-//       await loadCategorias(setCategorias);
-//     }
-    
-//     catch (error) {
-//       console.log(error);
-//     }
-//   }
-
-//   // useEffect(
-//   //   () => {
-//   //     setIsLogged(token)
-
-//   //     getCategories()
-
-//   //   },[]
-//   // )
-
-//   return (
-//     <>
-//       <div>
-//         <Navbar expand="lg" id="navbar">
-//           <Container fluid="md" id="container-nav">
-//             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-//             <Navbar.Brand href="/" id="nav-brand">
-//               <img src={logo} alt="logo" id="navbar-logo" />
-//             </Navbar.Brand>
-
-//             <Navbar.Collapse id="basic-navbar-nav">
-//               <div className="navbar-items ">
-//                 <Nav className="me-auto" id="teste">
-//                   <NavDropdown title="Categorias" id="basic-nav-dropdown">
-//                     {categorias.map((item) => {
-//                       console.log(item)
-//                       return (
-//                         <li key={item.id}>
-//                           <NavDropdown.Item href={`/`} id="categories-items">
-//                             {item.nome}
-//                           </NavDropdown.Item>
-//                         </li>
-//                       );
-//                     })}
-//                   </NavDropdown>
-//                   <Form>
-//                     <FormControl
-//                       type="search"
-//                       placeholder="Pesquisar cursos"
-//                       className="me-2"
-//                       aria-label="Search"
-//                       id="search-bar"
-//                       onKeyPress={() => console.log("Searching...")}
-//                     />
-//                   </Form>
-
-//                   <Nav.Link
-//                     href="/shopping-cart"
-//                     style={{ justifyContent: "center" }}
-//                     id="cart-link"
-//                   >
-//                     <BsFillCartFill style={{ fontSize: "24px" }} />
-//                     <span id="cart-count">
-//                       {cart.length === null ? 0 : cart.length}
-//                     </span>
-//                   </Nav.Link>
-
-//                   {!isLogged ? (
-//                     <>
-//                       <NavDropdown title="Minha Conta" id="basic-nav-dropdown">
-//                         <NavDropdown.Item
-//                           href={`/perfil`}
-//                           id="minhaconta-items"
-//                         >
-//                           Perfil
-//                         </NavDropdown.Item>
-//                         <NavDropdown.Item
-//                           href={`/my-courses`}
-//                           id="minhaconta-items"
-//                         >
-//                           Meus Cursos
-//                         </NavDropdown.Item>
-//                         <NavDropdown.Item
-//                           href={`/my-certificates`}
-//                           id="minhaconta-items"
-//                         >
-//                           Certificados
-//                         </NavDropdown.Item>
-//                         <NavDropdown.Item
-//                           href={`/my-payment-info`}
-//                           id="minhaconta-items"
-//                         >
-//                           Formas de Pagamento
-//                         </NavDropdown.Item>
-//                         <NavDropdown.Item
-//                           id="minhaconta-items"
-//                           onClick={() => logout()}
-//                         >
-//                           Sair
-//                         </NavDropdown.Item>
-//                       </NavDropdown>
-//                     </>
-//                   ) : (
-//                     <>
-//                       <Nav.Link
-//                         href="/signup"
-//                         className="nav-link"
-//                         id="signup-link"
-//                       >
-//                         Cadastre-se
-//                       </Nav.Link>
-//                       <Nav.Link
-//                         href="/login"
-//                         className="nav-link"
-//                         id="login-link"
-//                       >
-//                         Fazer Login
-//                       </Nav.Link>
-//                     </>
-//                   )}
-
-                  
-//                   {/* {user.role === "ADMIN" && (
-//                     <NavDropdown title="Admin" id="basic-nav-dropdown">
-//                       <NavDropdown.Item href={`/admin`} id="minhaconta-items">
-//                         Certificados
-//                       </NavDropdown.Item>
-
-//                       <NavDropdown.Item
-//                         id="minhaconta-items"
-//                         onClick={() => logout()}
-//                       >
-//                         Sair
-//                       </NavDropdown.Item>
-//                     </NavDropdown>
-//                   )} */}
-//                 </Nav>
-//               </div>
-//             </Navbar.Collapse>
-//           </Container>
-//         </Navbar>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default NavbarComponent;
 
 import React, { useEffect, useState } from "react";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
@@ -195,7 +24,6 @@ import ComboBox from "./comboBox.jsx";
 
 
 import cursoLoad from "../../services/curso/cursoLoad";
-import { Link } from "react-router-dom";
 
 function NavbarComponent() {
   const { cart } = useCart();
@@ -250,7 +78,7 @@ function NavbarComponent() {
           <Container fluid="md" id="container-nav">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-            <Navbar.Brand href={`/`} id="nav-brand">
+            <Navbar.Brand href="/" id="nav-brand">
               <img src={logo} alt="logo" id="navbar-logo" />
             </Navbar.Brand>
 
@@ -284,55 +112,43 @@ function NavbarComponent() {
                    
                   </Form>
 
-                  <Link
-                    to="shopping-cart"
+                  <Nav.Link
+                    href="/shopping-cart"
                     style={{ justifyContent: "center" }}
                     id="cart-link"
                   >
                     <BsFillCartFill style={{ fontSize: "24px" }} />
-                      <span id="cart-count">
-                        {cartLoaded?.length === null ? 0 : cartLoaded?.length}
-                      </span>
-
-                  </Link>
+                    <span id="cart-count">
+                      {cartLoaded?.length === null ? 0 : cartLoaded?.length}
+                    </span>
+                  </Nav.Link>
 
                   {token ? (
                     <>
                       <NavDropdown title="Minha Conta" id="basic-nav-dropdown">
                         <NavDropdown.Item
+                          href={`/perfil`}
                           id="minhaconta-items"
-                          >
-                          <Link
-                          to={`perfil`}
-                          >
+                        >
                           Perfil
-                          </Link>
                         </NavDropdown.Item>
                         <NavDropdown.Item
+                          href={`/my-courses`}
                           id="minhaconta-items"
-                          >
-                          <Link
-                          to={`my-courses`}
-                          >
-                            Meus Cursos
-                          </Link>
+                        >
+                          Meus Cursos
                         </NavDropdown.Item>
                         <NavDropdown.Item
+                          href={`/my-certificates`}
                           id="minhaconta-items"
-                          >
-                          <Link
-                          to={`my-certificates`}
-                          >
-                            Certificados
-                          </Link>
-
+                        >
+                          Certificados
                         </NavDropdown.Item>
                         <NavDropdown.Item
+                          href={`/my-payment-info`}
                           id="minhaconta-items"
-                          >
-                          <Link
-                          to={`my-payment-info`}
-                          >Formas de Pagamento</Link>
+                        >
+                          Formas de Pagamento
                         </NavDropdown.Item>
                         <NavDropdown.Item
                           id="minhaconta-items"
@@ -345,30 +161,18 @@ function NavbarComponent() {
                   ) : (
                     <>
                       <Nav.Link
-                        // href="/signup"
-                        href="#"
+                        href="/signup"
                         className="nav-link"
                         id="signup-link"
                       >
-
-                        <Link
-                          to="signup"
-                        >
-                          Cadastre-se
-                        </Link>
+                        Cadastre-se
                       </Nav.Link>
                       <Nav.Link
-                        // href="/login"
-                        href="#"
+                        href="/login"
                         className="nav-link"
                         id="login-link"
                       >
-                        <Link
-                        to="login"
-                        
-                        >
-                          Fazer Login
-                        </Link>
+                        Fazer Login
                       </Nav.Link>
                     </>
                   )}
