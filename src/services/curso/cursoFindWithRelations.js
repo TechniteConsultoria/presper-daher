e43ba9b3f55e1e40f107471e3c9567e6a5fe0ea9
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-21 10:43:41 -0300

    removed a log

--- src/services/curso/cursoFindWithRelations.js
+++ src/services/curso/cursoFindWithRelations.js
@@ -5,8 +5,6 @@ import { apiWithoutTenant } from "../api";
 export default async function cursoFindWithRelations(produtoId){
     return await apiWithoutTenant.get(`produto/${produtoId}`)
           .then((response) => {
-            console.log("response produto find")
-            console.log(response)
             let produtoData = response.data
             return produtoData
           });
