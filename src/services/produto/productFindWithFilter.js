3c6516284de45472cea83f90a8de38d9b11ed707
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-22 09:54:18 -0300

    now it filter correctly

--- src/services/produto/productFindWithFilter.js
+++ src/services/produto/productFindWithFilter.js
@@ -1,9 +1,9 @@
-import { api, apiWithTenantAndWithToken } from "../api";
+import { api, apiWithoutTenant, apiWithTenantAndWithToken } from "../api";
 
 
 
 export default async function productFindWithFilter(filter, value){
-    return await api.get(`produto?filter%5B${filter}%5D=${value}`)
+    return await apiWithoutTenant.get(`produto?filter%5B${filter}%5D=${value}`)
           .then((response) => {
             let productData = response.data.rows
             return productData
