19852bc9cd505390025c10b95cef208f3f6adafa
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-23 14:13:36 -0300

    fixed loading gif style

--- src/componentes/LoadingGif/index.js
+++ src/componentes/LoadingGif/index.js
@@ -3,9 +3,14 @@ import React from 'react'
 export default function LoadingGif() {
   return (
     <img
-        width="40px"
-        style={{ margin: "auto", display: 'flex',alignSelf: 'center' }}
-        height=""
+    style={{
+           width:"40px",
+           margin: "auto",
+           display: 'flex',
+           alignSelf: 'center',
+           height: "40px"
+          }}
+        height="40px"
         src={"https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"}
         alt="Loading"
         
